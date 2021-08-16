e6bcce7166a15d7817bd9ca272a422dd3321251b
WaveRider52 <saadetrecep@gmail.com>
2021-08-16 13:36:16 +0200

    Creating Shared Page Components

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,12 +1,17 @@
 import React from 'react'
 
+import Footer from '../components/footer'
+import Header from "../components/header"
+
 const ContactPage = () => {
     return (
         <div>
+            <Header />
             <h1>Contact</h1>
             <p>The best way to reach me is via 
                 <a href="https://twitter.com/@orcun_odabas" target="_blank" rel="noreferrer">@orcun_odabas</a> on Twitter!
             </p>
+            <Footer />
         </div>
     )
 }
