24e744d4271a3493c6107aea8de96cdf048aaef5
WaveRider52 <saadetrecep@gmail.com>
2021-08-27 20:47:55 +0200

    Styling Gatsby with CSS Modules

--- src/components/header.js
+++ src/components/header.js
@@ -1,28 +1,43 @@
 import React from 'react'
 import { Link } from 'gatsby'
 
+import * as headerStyles from './header.module.scss'
+
 const Header = () => {
     return (
-        <header>
-            <h1>Orçun Odabas</h1>
-            <nav>
-                <ul>
-                    <li>
-                        <Link to="/">Home</Link>
-                    </li>
-                    <li>
-                        <Link to="/blog">Blog</Link>
-                    </li>
-                    <li>
-                        <Link to="/about">About</Link>
-                    </li>
-                    <li>
-                        <Link to="/contact">Contact</Link>
-                    </li>
-                </ul>
-            </nav>
-        </header>
+      <header className={headerStyles.header}>
+        <h1>
+          <Link className={headerStyles.title} to="/">
+            Orçun Odabas
+          </Link>
+        </h1>
+        <nav>
+          <ul className={headerStyles.navList}>
+            <li>
+              <Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to="/">
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to="/blog">
+                Blog
+              </Link>
+            </li>
+            <li>
+              <Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to="/about">
+                About
+              </Link>
+            </li>
+            <li>
+              <Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to="/contact">
+                Contact
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
     )
-}
-
-export default Header
+  }
+  
+  export default Header
+  
